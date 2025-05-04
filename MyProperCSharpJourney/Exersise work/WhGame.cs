2cff4c505696ba0591b766b7f65a0a22b5467f87
IanSuenfa <iansuenfa2@gmail.com>
2025-05-04 15:59:02 +0100

    IM BACKKKKKKKKKK

--- MyProperCSharpJourney/Exersise work/WhGame.cs
+++ MyProperCSharpJourney/Exersise work/WhGame.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using MyProperCSharpJourney.Exisism;
 
 namespace MyProperCSharpJourney.Exersise_work
 {
@@ -14,14 +15,17 @@ namespace MyProperCSharpJourney.Exersise_work
         public string Name { get; }
         public int UnitAmount { get; }
         public int Health { get; private set; }
-        public int Strength { get; }
 
-        protected Unit(string name, int unitAmount, int strength, int health)
+        public int Toughness { get; private set; }
+        public int Saves { get; }
+
+        protected Unit(string name, int unitAmount, int saves, int toughness, int health)
         {
             Name = name;
             UnitAmount = unitAmount;
-            Strength = strength;
+            Saves = saves;
             Health = health;
+            Toughness = toughness; 
             _isAlive = health > 0;
         }
 
@@ -39,31 +43,89 @@ namespace MyProperCSharpJourney.Exersise_work
 
     public class Tyranid : Unit
     {
-        public Tyranid(string name, int unitAmount, int strength, int health)
-            : base(name, unitAmount, strength, health) { }
+        public Tyranid(string name, int unitAmount, int saves, int toughness, int health)
+            : base(name, unitAmount, saves, toughness, health) { }
 
         public static void Example()
         {
-            Tyranid leaper = new Tyranid("Leaper", 3, 5, 3);
+            Tyranid leaper = new Tyranid("Leaper", 3, 3, 5, 3);
             Console.WriteLine(leaper.Name);
             Console.WriteLine(leaper.UnitAmount);
-            Console.WriteLine(leaper.Strength);
+            Console.WriteLine(leaper.Saves);
+            Console.WriteLine(leaper.Toughness);
             Console.WriteLine(leaper.Health);
             Console.WriteLine(leaper.IsAlive());
+
+            Console.WriteLine("Choose your wepon");
+            Console.WriteLine("1. Leapers perail");
+            Console.WriteLine("Attacks: 6 ");
+            Console.WriteLine("Wounds: 4+");
+            Console.WriteLine("Strength: 5");
+            Console.WriteLine("Damage: 3");
+            Console.WriteLine("");
+            Console.WriteLine("2. Venemorph Scythes:");
+            Console.WriteLine("Attacks: 2 ");
+            Console.WriteLine("Wounds: 3+");
+            Console.WriteLine("Strength: 6");
+            Console.WriteLine("Damage: 4");
+ 
+            int TWepon = Convert.ToInt16(Console.ReadLine());
+            if (TWepon == 1)
+            {
+                Console.WriteLine("Leapers perail");
+                LeapersPerail.Example();
+
+            }
+            else if (TWepon == 2)
+            {
+                Console.WriteLine("Venemorph Scythes");
+            }
+            
+            else
+            {
+                Console.WriteLine("Invalid selection");
+            }
+
         }
+
+        public class LeapersPerail
+        {
+            public static void Example()
+            {
+                Tyranid leapers = new Tyranid("Leapers perail", 3, 3, 5, 3);
+                int TLPAttacks = 6 * leapers.UnitAmount;
+                int hitCounter = 0;
+                for (int i = 0; i < TLPAttacks; i++)
+                {
+                    int diceRoll = WhGame.RollDice();
+                    if (diceRoll >= 4)
+                    {
+                        hitCounter++;
+                    }
+                }
+                Console.WriteLine("Total hits:" + hitCounter);
+                Console.WriteLine(leapers.Saves);
+                Console.WriteLine(leapers.Toughness);
+                Console.WriteLine(leapers.Health);
+                Console.WriteLine(leapers.IsAlive());
+            }
+            
+        }
+
     }
 
     public class SpaceMarine : Unit
     {
-        public SpaceMarine(string name, int unitAmount, int strength, int health)
-            : base(name, unitAmount, strength, health) { }
+        public SpaceMarine(string name, int unitAmount, int saves, int toughness, int health)
+            : base(name, unitAmount, saves, toughness, health) { }
 
         public static void Example()
         {
-            SpaceMarine marine = new SpaceMarine("Marine", 4, 2, 4);
+            SpaceMarine marine = new SpaceMarine("Marine", 4, 5, 4, 3);
             Console.WriteLine(marine.Name);
             Console.WriteLine(marine.UnitAmount);
-            Console.WriteLine(marine.Strength);
+            Console.WriteLine(marine.Saves);
+            Console.WriteLine(marine.Toughness);
             Console.WriteLine(marine.Health);
             Console.WriteLine(marine.IsAlive());
         }
@@ -91,7 +153,18 @@ namespace MyProperCSharpJourney.Exersise_work
                 Console.WriteLine("Invalid selection");
             }
         }
-        
+
+        public static int RollDice()
+        {
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Dice roll: " + diceRoll);
+            return diceRoll;
+        }
     }
+
 }
+
+    
+
 
